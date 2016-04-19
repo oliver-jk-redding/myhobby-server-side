7c89c5fcbf2c725169536f112e631f39a06ab280
Ollie Redding <oliver.jk.redding@gmail.com>
2016-04-20 09:58:31 +1200

    updates

--- utils/get-user-info-util.js
+++ utils/get-user-info-util.js
@@ -3,7 +3,7 @@ var getUsers = require('./get-users-util');
 module.exports = (id, callback) => {
   getUsers(function(err, data) {
     if(err) {callback(err); return;}
-    for(user in data.users)
+    for(var user in data.users)
       if(data.users[user].id === id)
         callback(null, data.users[user]);
   })
--- utils/get-users-util.js
+++ utils/get-users-util.js
@@ -1,7 +1,9 @@
+'use strict'
+
 var fs = require('fs');
 var path = './db/users.json'
 
-module.exports = callback => {
+module.exports = (callback) => {
   fs.readFile(path, function(err, data) {
     if(err) {callback(err); return;}
     callback(null, JSON.parse(data.toString()));
